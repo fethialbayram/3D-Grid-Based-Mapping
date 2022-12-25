90d7a3d5a8e6421b063e51be444d253c7e74a92c
İpek Birinci <56261550+ipekbirinci@users.noreply.github.com>
2022-12-25 16:01:35 +0300

    update Transform.h
    header file a yorum eklendi

--- Transform.h
+++ Transform.h
@@ -1,24 +1,28 @@
-
+/*
+* @file Transform.h
+* @author Ipek Birinci (152120191020)
+* @date 23.12.2022
+* @brief Transform header file
+*
+*/
 #pragma once
 
 #include "PointCloud.h"
 #include <Eigen/Dense>
 
-/*Trans ve angle degerlerine gore donusum matrisini olusturur
-daha sonrasinda doTrans fonksiyonu ile alinan nokta ya da nokta bulutunu bu donusume tabi tutarak nokta bulutunu dondurur.
-*/
+
 class Transform{
 	Eigen::Vector3d angles;
 	Eigen::Vector3d trans;
 	Eigen::Matrix4d transMatrix;
 public:
-	Transform();
-	~Transform();
-	void setAngles(double, double, double);
-	void setTrans(double, double, double);
+	Transform(); //constructor
+	~Transform();//destructor
+	void setAngles(double, double, double);//aci değerlerini atayan fonk.
+	void setTrans(double, double, double);//transform değerlerini atayan fonk.
 	Eigen::Matrix4d getTransMatrix();
 	void setTransMatrix(Eigen::Matrix4d);
-	double* getAngles();
+	double* getAngles();//acilari getiren fonk.
 	double* getTrans();
 	list<Point> doTransForm(list<Point>points);
 	PointCloud doTransform(PointCloud);
